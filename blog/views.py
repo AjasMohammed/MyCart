6eac7b65516a83c0ce2c30c60d35ae334fc1dfd1
Ajas Mohammed <ajasmohammed33@gmail.com>
2022-10-22 12:27:58 +0530

    added for loop in blog.html, now the number of " slides depends upon number of posts in blogpost.html || thumbnail image added in the slides in blog page || in blogpost headings and contents showhs according to the inputs ||

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    mypost = Blogpost.objects.all()
+    return render(request, 'blog/index.html',
+                  {'mypost': mypost})
 
 
-def blogpost(request):
-    return render(request, 'blog/blogpost.html')
+def blogpost(request, id):
+    post = Blogpost.objects.filter(post_id=id)[0]
+    # print(post)
+    return render(request, 'blog/blogpost.html', {'post': post})
 
